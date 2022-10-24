086b2a8ebb67a4e5216b139cff298213e7388e13
Subs <frog2wah@gmail.com>
2022-10-24 13:39:04 +0200

    Rom: factor archives browsing

--- classes.py
+++ classes.py
@@ -16,33 +16,19 @@ class Rom:
 		self.romext = os.path.splitext(rom)[1][1:]
 		self.crc = crc
 		self.filecrc = filecrc
+		self.archiveContent = None
 
-	def getCRCFromZip(self) -> str:
-		with zipfile.ZipFile(self.rompathname) as romzip:
-			zipinfodata = romzip.infolist()
-			# We need only one file in the archive, useless otherwise
-			if len(zipinfodata) > 1:
-				return None
-			decimalCRC = romzip.getinfo(zipinfodata[0].filename).CRC
-			# Return the HEX value of the CRC, as getinfo returns a decimal value
-			return f'{decimalCRC:x}'
-
-	def getCRCFrom7z(self) -> str:
-		with py7zr.SevenZipFile(self.rompathname, 'r') as romzip:
-			zipinfodata = romzip.list()
-			if len(zipinfodata) > 1:
-				return None
-			decimalCRC = zipinfodata[0].crc32
-			# Return the HEX value of the CRC, as getinfo returns a decimal value
-			return f'{decimalCRC:x}'
+	def __repr__(self):
+		return "Rom({}, crc = {}, filecrc = {})".format(self.rompathname, self.crc, self.filecrc)
 
 	def getCRC(self) -> str:
 		if self.crc:
 			return self.crc
-		if self.romext == 'zip':
-			self.crc = self.getCRCFromZip()
-		if self.romext == '7z':
-			self.crc = self.getCRCFrom7z()
+		self.listArchive()
+		if len(self.archiveContent) == 1:
+			self.crc = list(self.archiveContent[0].values())[0]
+		else:
+			self.crc = None
 		return self.crc
 
 	def fileCRC(self) -> str:
@@ -51,5 +37,33 @@ class Rom:
 		self.filecrc = "%08X" % buf
 		return self.filecrc
 
-	def __repr__(self):
-		return "Rom({}, crc = {}, filecrc = {})".format(self.rompathname, self.crc, self.filecrc)
+	def listArchiveFromZip(self) -> list:
+		filesList = []
+		with zipfile.ZipFile(self.rompathname) as romzip:
+			zipinfodata = romzip.infolist()
+			for f in zipinfodata:
+				decimalCRC = romzip.getinfo(f.filename).CRC
+				filesList.append({f.filename: f'{decimalCRC:x}'})
+		return filesList
+
+	def listArchiveFrom7z(self) -> str:
+		filesList = []
+		with py7zr.SevenZipFile(self.rompathname, 'r') as romzip:
+			zipinfodata = romzip.list()
+			for f in zipinfodata:
+				decimalCRC = zipinfodata[0].crc32
+				filesList.append({f.filename: f'{decimalCRC:x}'})
+		return filesList
+
+	def listArchive(self) -> list:
+		if self.romext not in ['7z', 'zip']:
+			return None
+		if self.romext == 'zip':
+			self.archiveContent = self.listArchiveFromZip()
+		if self.romext == '7z':
+			self.archiveContent = self.listArchiveFrom7z()
+		print(self.archiveContent)
+
+	def extractRom(self, path='/tmp'):
+		if self.romext == 'zip':
+			pass
