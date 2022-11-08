bdda20bb7910b5dfc53c2f9c522140f7294b1bff
Subs <frog2wah@gmail.com>
2022-11-08 23:09:52 +0100

    Keep improving tests

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import requests
 import sys
 from dotenv import load_dotenv
 from os import environ as env
@@ -8,9 +9,20 @@ from scrapers.scraper import Scraper
 from scrapers.hfsdb import HFSDB
 from scrapers.screenscraper import ScreenScraper
 from scrapers.thegamesdb import TheGamesDb
-
-#Ugly, don't use that
 from classes.rom import Rom
+
+def downloadZips():
+	zips = { 'Sonic The Hedgehog 2 (World) (Rev A).7z': 'https://edgeemu.net/down.php?id=12698',
+		 'alienar.zip': 'https://www.mamedev.org/roms/alienar/alienar.zip'}
+	
+	for f, url in zips.items():
+		if os.path.exists('tests/' + f):
+			continue
+		r = requests.get(url, allow_redirects=True)
+		if not r.status_code == 200:
+			logging.warn("Couldn't download " + f)
+			continue
+		open('tests/' + f, 'wb').write(r.content)
 
 def test_Scraper():
 	myScraper = Scraper(name = 'ScraperTest',
@@ -29,29 +41,28 @@ def test_ScreenScraper():
 				user = env['SS_USER'],
 				password = env['SS_PASSWD'])
 	# print(vars(mySScraper))
-	#Sonic2Rom = Rom('~/Téléchargements/Sonic The Hedgehog 2 (World).zip')
-	Sonic2Rom = Rom('15353F104B97AB2A8F.zip')
+	Sonic2Rom = Rom('tests/Sonic The Hedgehog 2 (World) (Rev A).7z')
 	romInfo = mySScraper.getGameInfo(Sonic2Rom, 'megadrive')
 	logging.debug(romInfo.filterOnLang('fr'))
 	# print(mySScraper.download('ssuserInfos.php'))
-	# print(mySScraper.downloadToFile('sonic2_ss.json', 'jeuInfos.php', {'crc': '24ab4c3a'}))
+	# print(mySScraper.downloadToFile('tests/sonic2_ss.json', 'jeuInfos.php', {'crc': '24ab4c3a'}))
 	# mySScraper.getPlatforms()
 
 def test_TGDB():
 	myTGDBcraper = TheGamesDb(apiKey = env['TGDB_APIKEY'])
 	# print(vars(myTGDBcraper))
 	print(myTGDBcraper.download('v1/Games/ByGameID', {'id': 7504, 'fields': 'players,publishers,genres,overview,last_updated,rating,platform,coop,youtube,os,processor,ram,hdd,video,sound,alternates'}))
-	myTGDBcraper.downloadToFile('sonic2_tgdb.json', 'v1/Games/ByGameID', {'id': 7504, 'fields': 'players,publishers,genres,overview,last_updated,rating,platform,coop,youtube,os,processor,ram,hdd,video,sound,alternates'})
+	myTGDBcraper.downloadToFile('tests/sonic2_tgdb.json', 'v1/Games/ByGameID', {'id': 7504, 'fields': 'players,publishers,genres,overview,last_updated,rating,platform,coop,youtube,os,processor,ram,hdd,video,sound,alternates'})
 
 def test_HFSDB():
 	myHFSDB = HFSDB(user = env['HFSDB_USER'], password = env['HFSDB_PASSWD'])
 	print(myHFSDB.download('account'))
-	#Sonic2Rom = Rom('~/Téléchargements/Sonic The Hedgehog 2 (World) (Rev A).zip')
+	Sonic2Rom = Rom('tests/Sonic The Hedgehog 2 (World) (Rev A).7z')
 	# romInfo = myHFSDB.getGameInfo(Sonic2Rom)
-	myHFSDB.getPlatforms()
-	# myHFSDB.downloadToFile('sonic2_hfsdb.json', 'games', {'medias__md5': '9feeb724052c39982d432a7851c98d3e'})
-	# with open('sonic2_hfsdb.pretty.json', 'w') as f:
-		# json.dump(json.loads(myHFSDB.download('games', {'medias__md5': '9feeb724052c39982d432a7851c98d3e'})['content']), f, indent=4)
+	# myHFSDB.getPlatforms()
+	myHFSDB.downloadToFile('tests/sonic2_hfsdb.json', 'games', {'medias__md5': Sonic2Rom.md5sum})
+	with open('tests/sonic2_hfsdb.pretty.json', 'w') as f:
+		json.dump(json.loads(myHFSDB.download('games', {'medias__md5': '9feeb724052c39982d432a7851c98d3e'})['content']), f, indent=4)
 
 loggingLevel = logging.DEBUG
 if loggingLevel == logging.DEBUG:
@@ -63,8 +74,9 @@ else:
 if not load_dotenv(verbose=True):
 	logging.warn("Couldn't load_dotenv()")
 
+downloadZips()
 test_Scraper()
-# test_ScreenScraper()
+test_ScreenScraper()
 # test_TGDB()
 test_HFSDB()
 
