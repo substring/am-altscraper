f1d3491222e9f517b1bbf880b3337e633f70f2fb
Subs <substring@users.noreply.github.com>
2022-11-10 09:03:15 +0000

    Linting step 1

--- test.py
+++ test.py
@@ -1,31 +1,38 @@
+"""Tests for possible scrapers
+"""
 import json
 import logging
 import os
-import requests
 import sys
-from dotenv import load_dotenv
 from os import environ as env
-from scrapers.scraper import Scraper
+
+import requests
+from dotenv import load_dotenv
+
+from classes.rom import Rom
 from scrapers.hfsdb import HFSDB
+from scrapers.scraper import Scraper
 from scrapers.screenscraper import ScreenScraper
 from scrapers.thegamesdb import TheGamesDb
-from classes.rom import Rom
 
-def downloadZips():
+
+def download_zips():
+	"""Downloads various romfiles for testing"""
 	zips = { 'Sonic The Hedgehog 2 (World) (Rev A).7z': 'https://edgeemu.net/down.php?id=12698',
-		 'alienar.zip': 'https://www.mamedev.org/roms/alienar/alienar.zip'}
-	
+			'alienar.zip': 'https://www.mamedev.org/roms/alienar/alienar.zip'}
+
 	for f, url in zips.items():
 		if os.path.exists('tests/' + f):
 			continue
-		r = requests.get(url, allow_redirects=True)
+		r = requests.get(url, allow_redirects=True, timeout=15)
 		if not r.status_code == 200:
-			logging.warn("Couldn't download " + f)
+			logging.warning("Couldn't download %s", f)
 			continue
 		open('tests/' + f, 'wb').write(r.content)
 
-def test_Scraper():
-	myScraper = Scraper(name = 'ScraperTest',
+	def test_scraper():
+	"""Test the Scraper base class"""
+	my_scraper = Scraper(name = 'ScraperTest',
 				baseUrl = 'http://google.fr',
 				baseUrlParams = 'lang=fr',
 				apiKey = '4pIk3y',
@@ -33,54 +40,57 @@ def test_Scraper():
 				devPassword = 'somePass',
 				user = 'anotherUser',
 				password = 'anotherPass')
-	# print(vars(myScraper))
+	logging.info(vars(my_scraper))
 
-def test_ScreenScraper():
-	mySScraper = ScreenScraper(devUser = env['SS_DEVUSER'],
+def test_screenscraper():
+	"""Test the ScreenScraper Scraper class"""
+	my_sscraper = ScreenScraper(devUser = env['SS_DEVUSER'],
 				devPassword = env['SS_DEVPASSWD'],
 				user = env['SS_USER'],
 				password = env['SS_PASSWD'])
-	# print(vars(mySScraper))
-	Sonic2Rom = Rom('tests/Sonic The Hedgehog 2 (World) (Rev A).7z')
-	romInfo = mySScraper.getGameInfo(Sonic2Rom, 'megadrive')
-	logging.debug(romInfo.filterOnLang('fr'))
-	# print(mySScraper.download('ssuserInfos.php'))
-	# print(mySScraper.downloadToFile('tests/sonic2_ss.json', 'jeuInfos.php', {'crc': '24ab4c3a'}))
-	# mySScraper.getPlatforms()
+	# print(vars(my_sscraper))
+	sonic2_rom = Rom('tests/Sonic The Hedgehog 2 (World) (Rev A).7z')
+	rom_info = my_sscraper.getGameInfo(sonic2_rom, 'megadrive')
+	logging.debug(rom_info.filterOnLang('fr'))
+	# print(my_sscraper.download('ssuserInfos.php'))
+	# print(my_sscraper.downloadToFile('tests/sonic2_ss.json', 'jeuInfos.php', {'crc': '24ab4c3a'}))
+	# my_sscraper.getPlatforms()
 
-def test_TGDB():
-	myTGDBcraper = TheGamesDb(apiKey = env['TGDB_APIKEY'])
-	# print(vars(myTGDBcraper))
-	print(myTGDBcraper.download('v1/Games/ByGameID', {'id': 7504, 'fields': 'players,publishers,genres,overview,last_updated,rating,platform,coop,youtube,os,processor,ram,hdd,video,sound,alternates'}))
-	myTGDBcraper.downloadToFile('tests/sonic2_tgdb.json', 'v1/Games/ByGameID', {'id': 7504, 'fields': 'players,publishers,genres,overview,last_updated,rating,platform,coop,youtube,os,processor,ram,hdd,video,sound,alternates'})
+def test_tgdb():
+	"""Test the TGDB Scraper class"""
+	my_tgdbcraper = TheGamesDb(apiKey = env['TGDB_APIKEY'])
+	# print(vars(my_tgdbcraper))
+	print(my_tgdbcraper.download('v1/Games/ByGameID', {'id': 7504, 'fields': 'players,publishers,genres,overview,last_updated,rating,platform,coop,youtube,os,processor,ram,hdd,video,sound,alternates'}))
+	my_tgdbcraper.downloadToFile('tests/sonic2_tgdb.json', 'v1/Games/ByGameID', {'id': 7504, 'fields': 'players,publishers,genres,overview,last_updated,rating,platform,coop,youtube,os,processor,ram,hdd,video,sound,alternates'})
 
-def test_HFSDB():
-	myHFSDB = HFSDB(user = env['HFSDB_USER'], password = env['HFSDB_PASSWD'])
-	print(myHFSDB.download('account'))
-	Sonic2Rom = Rom('tests/Sonic The Hedgehog 2 (World) (Rev A).7z')
-	romInfo = myHFSDB.getGameInfo(Sonic2Rom)
-	logging.debug(romInfo.filterOnLang('fr'))
-	# myHFSDB.getPlatforms()
-	myHFSDB.downloadToFile('tests/sonic2_hfsdb.json', 'games', {'medias__md5': Sonic2Rom.md5sum})
+def test_hfsdb():
+	""" Test the HFSDB Scraper class"""
+	my_hfsdb = HFSDB(user = env['HFSDB_USER'], password = env['HFSDB_PASSWD'])
+	print(my_hfsdb.download('account'))
+	sonic2_rom = Rom('tests/Sonic The Hedgehog 2 (World) (Rev A).7z')
+	rom_info = my_hfsdb.getGameInfo(sonic2_rom)
+	logging.debug("Filtered som:\n%s", rom_info.filterOnLang('fr'))
+	# my_hfsdb.getPlatforms()
+	my_hfsdb.downloadToFile('tests/sonic2_hfsdb.json', 'games', {'medias__md5': sonic2_rom.md5sum})
 	with open('tests/sonic2_hfsdb.pretty.json', 'w') as f:
-		json.dump(json.loads(myHFSDB.download('games', {'medias__md5': '9feeb724052c39982d432a7851c98d3e'})['content']), f, indent=4)
+		json.dump(json.loads(my_hfsdb.download('games', {'medias__md5': '9feeb724052c39982d432a7851c98d3e'})['content']), f, indent=4)
 	with open('tests/alienar_hfsdb.pretty.json', 'w') as f:
-		json.dump(json.loads(myHFSDB.download('games', {'medias__description': 'alienar'})['content']), f, indent=4)
+		json.dump(json.loads(my_hfsdb.download('games', {'medias__description': 'alienar'})['content']), f, indent=4)
 
-loggingLevel = logging.DEBUG
-if loggingLevel == logging.DEBUG:
-	logging.basicConfig(stream=sys.stdout, level=loggingLevel, datefmt='%Y-%m-%d %H:%M:%S',
+LOGGING_LEVEL = logging.DEBUG
+if LOGGING_LEVEL == logging.DEBUG:
+	logging.basicConfig(stream=sys.stdout, level=LOGGING_LEVEL, datefmt='%Y-%m-%d %H:%M:%S',
 		format='[%(levelname)s] %(filename)s/%(funcName)s(%(lineno)d): %(message)s')
 else:
-	logging.basicConfig(stream=sys.stdout, level=loggingLevel)
+	logging.basicConfig(stream=sys.stdout, level=LOGGING_LEVEL)
 
 if not load_dotenv(verbose=True):
-	logging.warn("Couldn't load_dotenv()")
+	logging.warning("Couldn't load_dotenv()")
 
-downloadZips()
-test_Scraper()
-test_ScreenScraper()
-# test_TGDB()
-test_HFSDB()
+download_zips()
+test_scraper()
+test_screenscraper()
+# test_tgdb()
+test_hfsdb()
 
 #####NEED TO TEST ARCADE AND CONSOLE ROM
--- test_frontends.py
+++ test_frontends.py
@@ -1,24 +1,27 @@
 import logging
 import sys
 
-from frontends.frontend import FrontEnd
 from frontends.attractmode import AttractMode
+from frontends.frontend import FrontEnd
 
 
-def test_FrontEnd():
-	myFE = FrontEnd(name='Test FE', system='Megadrive', cfgFile='test/MAME.cfg', romsDir='/roms', extensions=['zip', '7z'], artworkPath='/medias')
-	logging.debug('\n' + str(myFE))
+def test_frontend():
+    """Test the FrontEnd base class"""
+    my_fe = FrontEnd(name='Test FE', system='Megadrive', cfgFile='test/MAME.cfg', romsDir='/roms',
+        extensions=['zip', '7z'], artworkPath='/medias')
+    logging.debug("\n%s", my_fe)
 
-def test_AttractMode():
-	myFE = AttractMode(system='arcade', cfgFile='tests/MAME.cfg')
-	logging.debug('\n' + str(myFE))
+def test_attractmode():
+    """Test the AttractMode Frontend child class"""
+    my_fe = AttractMode(system='arcade', cfgFile='tests/MAME.cfg')
+    logging.debug("\n%s", my_fe)
 
-loggingLevel = logging.DEBUG
-if loggingLevel == logging.DEBUG:
-	logging.basicConfig(stream=sys.stdout, level=loggingLevel, datefmt='%Y-%m-%d %H:%M:%S',
-		format='[%(levelname)s] %(filename)s/%(funcName)s(%(lineno)d): %(message)s')
+LOGGING_LEVEL = logging.DEBUG
+if LOGGING_LEVEL == logging.DEBUG:
+    logging.basicConfig(stream=sys.stdout, level=LOGGING_LEVEL, datefmt='%Y-%m-%d %H:%M:%S',
+        format='[%(levelname)s] %(filename)s/%(funcName)s(%(lineno)d): %(message)s')
 else:
-	logging.basicConfig(stream=sys.stdout, level=loggingLevel)
+    logging.basicConfig(stream=sys.stdout, level=LOGGING_LEVEL)
 
-test_FrontEnd()
-test_AttractMode()
+test_frontend()
+test_attractmode()
